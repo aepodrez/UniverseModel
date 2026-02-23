759abf7a2fe23997a5db23fd053b995aa59c548e
Alex Podrez <aepodrez@mit.edu>
2026-02-23 02:53:09 +0000

    Move AWS provider to provider.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 # Data sources for account and region information
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
